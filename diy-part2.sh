9d6cc3af77cc972b120e8808630c322f995702f6
thangcualo <122159126+thangcualo@users.noreply.github.com>
2023-10-02 00:57:18 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -26,39 +26,3 @@ sed -i "42i echo 'iptables -t nat -I POSTROUTING -o eth0.1 -j MASQUERADE' >> /et
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 # Modify default theme
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-# Rooter Support untuk modem rakitan
-svn co https://github.com/thangcualo/ROOterSource2102/trunk/package/rooter-builds/0protocols/luci-proto-3x package/luci-proto-3x
-svn co https://github.com/thangcualo/ROOterSource2102/trunk/package/rooter-builds/0protocols/luci-proto-mbim package/luci-proto-mbim
-svn co https://github.com/thangcualo/ROOterSource2102/trunk/package/rooter/0drivers/rmbim package/rmbim
-svn co https://github.com/thangcualo/ROOterSource2102/trunk/package/rooter/0drivers/rqmi package/rqmi
-svn co https://github.com/thangcualo/ROOterSource2102/trunk/package/rooter/0basicsupport/ext-sms package/ext-sms
-svn co https://github.com/thangcualo/ROOterSource2102/trunk/package/rooter/0basicsupport/ext-buttons package/ext-buttons
-svn co https://github.com/thangcualo/ROOterSource2102/trunk/package/rooter/ext-rooter-basic package/ext-rooter-basic
-# Rooter splash
-svn co https://github.com/karnadii/rooter/trunk/package/rooter/0splash/status package/status
-svn co https://github.com/karnadii/rooter/trunk/package/rooter/0splash/splash package/splash
-svn co https://github.com/karnadii/rooter/trunk/package/rooter/0splash/ext-splashconfig package/ext-splashconfig
-svn co https://github.com/karnadii/rooter/trunk/package/rooter/0splash/ext-splash package/ext-splash
-# Rooter Bandwith monitor
-svn co https://github.com/karnadii/rooter/trunk/package/rooter/0optionalapps/bwallocate package/bwallocate
-svn co https://github.com/karnadii/rooter/trunk/package/rooter/0optionalapps/bwmon package/bwmon
-svn co https://github.com/karnadii/rooter/trunk/package/rooter/0optionalapps/ext-throttle package/ext-throttle
-
-# disable banner from rooter
-sudo chmod -x package/ext-rooter-basic/files/etc/init.d/bannerset
-sed -i 's/luci-theme-openwrt-2020/luci-theme-argon/g' package/ext-rooter-basic/Makefile
-# Add luci-app-atinout-mod
-svn co https://github.com/4IceG/luci-app-atinout-mod/trunk package/luci-app-atinout-mod
-
-# internet detector
-svn co https://github.com/gSpotx2f/luci-app-internet-detector/trunk/luci-app-internet-detector package/luci-app-internet-detector
-svn co https://github.com/gSpotx2f/luci-app-internet-detector/trunk/internet-detector package/internet-detector
-# iStore
-svn co https://github.com/linkease/istore-ui/trunk/app-store-ui package/app-store-ui
-svn co https://github.com/linkease/istore/trunk/luci package/istore
-# Set modemmanager to disable
-mkdir -p feeds/luci/protocols/luci-proto-modemmanager/root/etc/uci-defaults
-cat << EOF > feeds/luci/protocols/luci-proto-modemmanager/root/etc/uci-defaults/70-modemmanager
-[ -f /etc/init.d/modemmanager ] && /etc/init.d/modemmanager disable
-exit 0
-EOF
